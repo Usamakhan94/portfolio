346ccef1673ac9534eca5baf5d0ff68e2638be07
Usamakhan94 <usama_khan1994@live.com>
2024-06-15 04:41:29 +0500

    Homepage progress

--- components/section/Home/Hero.tsx
+++ components/section/Home/Hero.tsx
@@ -58,17 +58,28 @@ const Hero = () => {
           </motion.h1>
         </MotionConfig>
         <div className="flex flex-col justify-center items-center my-10">
-          <div className=" animate-bounce delay-900 duration-1000">
+          <div className=" animate-bounce duration-1000">
             <ScrollIcon />
           </div>
           <h6 className=" text-base font-semibold mt-2">Scroll</h6>
+        </div>
+        <div
+          className=" w-[180px] h-[180px] !bg-no-repeat !bg-cover absolute top-[5%] right-[20%]"
+          style={{ background: "url(/inner-logo.png)" }}
+        >
+          <Image
+            src="/outer-logo.png"
+            alt=""
+            fill
+            className="!static animate-spin duration-full"
+          />
         </div>
       </div>
       <div className="pointer-events-none bg-[#3C1477] rounded-full absolute -z-10 -top-[5%] left-[40%] -translate-x-1/2 -translate-y-1/2 w-[51.875rem] h-[51.875rem] blur-[150px] before:bg-[#4C0E49] before:rounded-full before:absolute before:-z-20 before:top-[40%] before:-right-[30%] before:-translate-x-1/2 before:-translate-y-1/2 before:w-[37.5rem] before:h-[37.5rem] after:bg-[#0AA4F8] after:rounded-full after:absolute after:-z-20 after:top-[60%] after:-right-[55%] after:w-[25rem] after:h-[25rem]" />
       <div className="absolute w-[1200px] h-[700px] pointer-events-none top-[15%] -z-10 -left-[5%]">
         <Image
           className="object-contain"
-          src="/main_bnr.png"
+          src="/main_bnr.webp"
           alt="Bg Curve"
           fill
         />
